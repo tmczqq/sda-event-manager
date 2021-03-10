57edf56a0750291403a035a898e46ce89da26372
tomasz <tomczak111@gmail.com>
2021-03-10 21:09:31 +0100

    Feature-1.2 ready

--- event_manager/urls.py
+++ event_manager/urls.py
@@ -7,4 +7,9 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('event_manager_app.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    url(r"^register/", include("django.contrib.auth.urls"), name='password_change'),
+
+]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
