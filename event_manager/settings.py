e8eab5f5c12b9c57705fea7f669dee01cb92d2b7
tomasz <tomczak111@gmail.com>
2021-03-12 20:39:50 +0100

    add allowed hosts

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['https://sdaeventmanager.herokuapp.com/','localhost:8000']
 
 # Application definition
 
