cfb9a6a4e2fa70b27efa7f580968400184c7415b
tomasz <tomczak111@gmail.com>
2021-03-13 10:22:36 +0100

    fix settings

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://sdaeventmanager.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://sdaeventmanager.herokuapp.com/','sdaeventmanager.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
